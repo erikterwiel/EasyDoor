630e62397e3b43e84cd646edb2b2842c29c742c6
ehterwie <ehterwie@uwaterloo.ca>
2017-11-03 23:44:04 -0400

    ResidentAddActivity now takes pictures

--- EasyDoorApp/app/src/main/java/erikterwiel/easydoorapp/ResidentAddActivity.java
+++ EasyDoorApp/app/src/main/java/erikterwiel/easydoorapp/ResidentAddActivity.java
@@ -1,15 +1,22 @@
 package erikterwiel.easydoorapp;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
+import java.util.ArrayList;
+
 public class ResidentAddActivity extends AppCompatActivity {
 
-    private final static String TAG = "ResidentAddActivity.class";
+    private static final String TAG = "ResidentAddActivity.class";
+    private static final int REQUEST_IMAGE_CAPTURE = 100;
 
+    private ArrayList<Bitmap> mBitmaps = new ArrayList<>();
     private ImageView mAddPhotoButton;
 
     @Override
@@ -23,9 +30,27 @@ public class ResidentAddActivity extends AppCompatActivity {
         mAddPhotoButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                takePicture();
             }
         });
+    }
+
+    private void takePicture() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = intent.getExtras();
+            mBitmaps.add((Bitmap) extras.get("data"));
+            if (mBitmaps.size() != 5) {
+                takePicture();
+            }
+        }
     }
 
 
