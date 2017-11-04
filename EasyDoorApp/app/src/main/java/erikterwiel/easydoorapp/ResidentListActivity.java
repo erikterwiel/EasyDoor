3c3dc3158dfc947c1794f55e121b50c63e4b0ed9
ehterwie <ehterwie@uwaterloo.ca>
2017-11-03 22:00:11 -0400

    Initial commit

---
+++ EasyDoorApp/app/src/main/java/erikterwiel/easydoorapp/ResidentListActivity.java
@@ -0,0 +1,13 @@
+package erikterwiel.easydoorapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ResidentListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resident_list);
+    }
+}
