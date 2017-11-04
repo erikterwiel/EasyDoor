22e3c9ee9395d6a244388885a78809f6f8308b57
ehterwie <ehterwie@uwaterloo.ca>
2017-11-04 18:18:27 -0400

    MainActivity on camera can now successfully connect to Rekognition

--- EasyDoorCamera/app/src/main/java/com/example/jasonxian/easydoortest/MainActivity.java
+++ EasyDoorCamera/app/src/main/java/com/example/jasonxian/easydoortest/MainActivity.java
@@ -1,108 +1,164 @@
 package com.example.jasonxian.easydoortest;
 
+import android.Manifest;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
 import com.amazonaws.AmazonClientException;
 import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.CognitoCachingCredentialsProvider;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferUtility;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.rekognition.AmazonRekognition;
-import com.amazonaws.services.rekognition.AmazonRekognitionClientBuilder;
+import com.amazonaws.services.rekognition.AmazonRekognitionClient;
+import com.amazonaws.services.rekognition.model.Attribute;
 import com.amazonaws.services.rekognition.model.CompareFacesMatch;
 import com.amazonaws.services.rekognition.model.CompareFacesRequest;
 import com.amazonaws.services.rekognition.model.CompareFacesResult;
+import com.amazonaws.services.rekognition.model.DetectFacesRequest;
 import com.amazonaws.services.rekognition.model.DetectLabelsRequest;
 import com.amazonaws.services.rekognition.model.DetectLabelsResult;
 import com.amazonaws.services.rekognition.model.Image;
 import com.amazonaws.services.rekognition.model.Label;
 import com.amazonaws.services.rekognition.model.S3Object;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
+import com.amazonaws.util.IOUtils;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Array;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
+    private static final String COGNITO_POOL_ID = "us-east-1:a0f4188d-f4cd-45f5-920b-a6a942b4721c";
+    private static final String COGNITO_POOL_REGION = "us-east-1";
+    private static final String BUCKET_NAME = "picturedatabase";
+    private static final String BUCKET_REGION = "us-east-1";
+    private static final String PROVIDER_AUTHORITY = "erikterwiel.easydoorcamera.fileprovider";
+    private static final int REQUEST_CAMERA = 100;
+    private static final float SIMILARITY_THRESHOLD = 70F;
+
+    private TransferUtility mTransferUtility;
+    private AmazonRekognitionClient mAmazonRekognitionClient;
+    private AWSCredentialsProvider mCredentialsProvider;
+    private String mImagePath;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        float similarityThreshold = 70F;
-        String sourceImage = "doge.jpg";
-        String targetImage = "mansour2.jpg";
-        String bucket = "easydoor2";
-        int numSuspects = 6;
-        boolean foundSuspect = false;
-        boolean possibleDanger = false;
 
-        AWSCredentials credentials;
-        try {
-            credentials = new BasicAWSCredentials("AKIAIDS2JKXNRSWWJG4A",",wR+MzQU3SW7eDKK0s5uodUEvFPP8lE0B8BcDSDGe");
-        } catch (Exception e) {
-            throw new AmazonClientException("Pleases check for valid credentials", e);
+        ActivityCompat.requestPermissions(this, new String[] {
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.READ_EXTERNAL_STORAGE}, 101);
+
+        mTransferUtility = getTransferUtility(this);
+        mAmazonRekognitionClient = new AmazonRekognitionClient(mCredentialsProvider);
+
+        launchCamera();
+    }
+
+    private void launchCamera() {
+        File file = getFile();
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, FileProvider.getUriForFile(
+                MainActivity.this, PROVIDER_AUTHORITY, file));
+        startActivityForResult(cameraIntent, REQUEST_CAMERA);
+    }
+
+    private File getFile() {
+        File folder = new File("sdcard/Pictures/EasyDoor");
+        if (!folder.exists()) folder.mkdir();
+        File image = new File(
+                folder, "input.jpg");
+        mImagePath = image.getAbsolutePath();
+        return image;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        new CompareFace().execute();
+    }
+
+    public TransferUtility getTransferUtility(Context context) {
+        TransferUtility sTransferUtility = new TransferUtility(
+                getS3Client(context.getApplicationContext()), context.getApplicationContext());
+        return sTransferUtility;
+    }
+
+    public AmazonS3Client getS3Client(Context context) {
+        AmazonS3Client sS3Client = new AmazonS3Client(getCredProvider(context.getApplicationContext()));
+        return sS3Client;
+    }
+
+    private AWSCredentialsProvider getCredProvider(Context context) {
+        mCredentialsProvider = new CognitoCachingCredentialsProvider(
+                context.getApplicationContext(),
+                COGNITO_POOL_ID,
+                Regions.fromName(COGNITO_POOL_REGION));
+        return mCredentialsProvider;
+    }
+
+    private class CompareFace extends AsyncTask<Void, Void, Void> {
+        private ProgressDialog dialog;
+
+        @Override
+        protected void onPreExecute() {
+            dialog = ProgressDialog.show(MainActivity.this,
+                    getString(R.string.refreshing),
+                    getString(R.string.please_wait));
         }
 
-         DetectLabelsRequest labelsRequest = new DetectLabelsRequest()
-                .withImage(new Image()
-                        .withS3Object(new S3Object()
-                                .withName(sourceImage).withBucket(bucket)))
-                .withMaxLabels(15)
-                .withMinConfidence(50F);
-
-        AmazonRekognition rekognitionClient = AmazonRekognitionClientBuilder
-                .standard()
-                .withRegion(Regions.US_EAST_1)
-                .withCredentials(new AWSStaticCredentialsProvider(credentials))
-                .build();
-        try {
-            DetectLabelsResult result = rekognitionClient.detectLabels(labelsRequest);
-            List<Label> labels = result.getLabels();
-            Log.i("info","Detected labels for " + sourceImage);
-            for (Label label: labels) {
-                Log.i("info",label.getName() + ": " + label.getConfidence().toString());
-                if((label.getName().equals("Fork") || label.getName().equals("Gun"))) possibleDanger = true;
+        @Override
+        protected Void doInBackground(Void... inputs) {
+            try {
+                InputStream inputStream = new FileInputStream(mImagePath);
+                ByteBuffer imageBytes = ByteBuffer.wrap(IOUtils.toByteArray(inputStream));
+                Image image = new Image().withBytes(imageBytes);
+                DetectLabelsRequest detectLabelsRequest = new DetectLabelsRequest()
+                        .withImage(image)
+                        .withMaxLabels(10)
+                        .withMinConfidence(75F);
+                DetectLabelsResult detectLabelsResult =
+                        mAmazonRekognitionClient.detectLabels(detectLabelsRequest);
+                List<Label> labels = detectLabelsResult.getLabels();
+                for (int i = 0; i < labels.size(); i++) {
+                    Log.i(TAG, labels.get(i).getName() + ":" + labels.get(i).getConfidence().toString());
+                }
+            } catch (Exception ex) {
+                ex.printStackTrace();
             }
-        } catch(Exception e) {
-            e.printStackTrace();
+            return null;
         }
-        Image source = new Image().withS3Object(new S3Object().withBucket(bucket).withName(sourceImage));
-        Image target = new Image().withS3Object(new S3Object().withBucket(bucket).withName(targetImage));
 
-        CompareFacesRequest request = new CompareFacesRequest()
-                .withSourceImage(source)
-                .withTargetImage(target)
-                .withSimilarityThreshold(similarityThreshold);
-        try{
-            CompareFacesResult compareFacesResult = rekognitionClient.compareFaces(request);
-            List <CompareFacesMatch> faceDetails = compareFacesResult.getFaceMatches();
-            if(!faceDetails.isEmpty()){
-                System.out.println("Match, opening door.");
-            }else{
-                for(int i = 1; i < numSuspects; i++){
-                    targetImage = "suspect" + i + ".jpg";
-                    target = new Image().withS3Object(new S3Object().withBucket(bucket).withName(targetImage));
-                    request = new CompareFacesRequest()
-                            .withSourceImage(source)
-                            .withTargetImage(target)
-                            .withSimilarityThreshold(similarityThreshold);
-                    compareFacesResult = rekognitionClient.compareFaces(request);
-                    faceDetails = compareFacesResult.getFaceMatches();
-                    if(!faceDetails.isEmpty()){
-                        foundSuspect = true;
-                        break;
-                    }
-                }
-                if(foundSuspect){
-                    System.out.println("Calling police...");
-                }else if(possibleDanger){
-                    System.out.println("Visitor could have dangerous weapons ...");
-                }else{
-                    System.out.println("Retake photo or nothing is there.");
-                }
-            }
-        }catch(Exception e){
-            System.out.println("No face comparsion found, take a new picture.");
+        @Override
+        protected void onPostExecute(Void result) {
+            dialog.dismiss();
         }
     }
 }
