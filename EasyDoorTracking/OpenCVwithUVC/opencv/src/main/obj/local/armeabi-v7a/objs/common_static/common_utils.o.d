762f4678e19409860b04feb052fbaf7b2f584051
ehterwie <ehterwie@uwaterloo.ca>
2017-11-06 11:39:12 -0500

    Removed unused library

--- EasyDoorTracking/OpenCVwithUVC/opencv/src/main/obj/local/armeabi-v7a/objs/common_static/common_utils.o.d
+++
@@ -1,9 +0,0 @@
-./obj/local/armeabi-v7a/objs/common_static/common_utils.o: \
-  jni/common/common_utils.cpp jni/common/..\utilbase.h \
-  jni/common/../localdefines.h jni/common/common_utils.h
-
-jni/common/..\utilbase.h:
-
-jni/common/../localdefines.h:
-
-jni/common/common_utils.h:
