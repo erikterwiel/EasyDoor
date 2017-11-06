100f9a5d66425281367c5c24cd3aad1905f82ada
ehterwie <ehterwie@uwaterloo.ca>
2017-11-06 11:41:12 -0500

    Revert "Removed unused library"
    This reverts commit 762f4678e19409860b04feb052fbaf7b2f584051.

---
+++ EasyDoorTracking/OpenCVwithUVC/opencv/src/main/obj/local/x86/objs/common_static/JNIHelp.o.d
@@ -0,0 +1,14 @@
+./obj/local/x86/objs/common_static/JNIHelp.o: jni/common/JNIHelp.cpp \
+  jni/common/..\utilbase.h jni/common/../localdefines.h \
+  jni/common/JniConstants.h jni/common/JNIHelp.h \
+  C:/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\string
+
+jni/common/..\utilbase.h:
+
+jni/common/../localdefines.h:
+
+jni/common/JniConstants.h:
+
+jni/common/JNIHelp.h:
+
+C:/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\string:
---
+++ EasyDoorTracking/OpenCVwithUVC/opencv/src/main/obj/local/x86/objs/common_static/common_utils.o.d
@@ -0,0 +1,9 @@
+./obj/local/x86/objs/common_static/common_utils.o: \
+  jni/common/common_utils.cpp jni/common/..\utilbase.h \
+  jni/common/../localdefines.h jni/common/common_utils.h
+
+jni/common/..\utilbase.h:
+
+jni/common/../localdefines.h:
+
+jni/common/common_utils.h:
